4bbd7d9736e83ad7af24f4ddf7359974b1964ade
RoySanchez111 <royismael2022@outlook.com>
2026-04-20 21:55:06 -0600

    Actualizacion

--- app/app.py
+++ app/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
-    return jsonify({"message": "¡Hola desde Flask en Docker!"})
+    # Hemos actualizado el mensaje para validar el despliegue automático
+    return jsonify({"message": "¡Actualización exitosa V2! El Pipeline de Jenkins funciona perfecto."})
 
 if __name__ == "__main__":
+    # Escucha en el puerto 5000 para que coincida con tu configuración de Docker y AWS [cite: 14, 21]
     app.run(host="0.0.0.0", port=5000)
